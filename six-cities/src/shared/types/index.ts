a64a925cf66265db197e33557a44ab4f4c868345
kirill-cite <kirill.kysh06041996@yandex.ru>
2024-11-15 18:04:28 +0300

    1.12 Добавит интерфейс FileReader

---
+++ six-cities/src/shared/types/index.ts
@@ -0,0 +1,6 @@
+export { City} from './city.type.js';
+export { Comment } from './comment.type.js';
+export { Location } from './location.type.js';
+export { Offer } from './offer.type.js';
+export { User } from './user.type.js';
+
