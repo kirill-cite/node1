935525365c7b1a6db6899c9bf4eb2b0be881025d
kirill-cite <kirill.kysh06041996@yandex.ru>
2024-11-30 14:33:54 +0300

    2.5 Добавит тип MockServerData

--- six-cities/src/shared/types/index.ts
+++ six-cities/src/shared/types/index.ts
@@ -3,4 +3,6 @@ export { Comment } from './comment.type.js';
 export { Location } from './location.type.js';
 export { Offer } from './offer.type.js';
 export { User } from './user.type.js';
+export { MockServerData } from './mock-server-data.type.js';
+
 
---
+++ six-cities/src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,10 @@
+export type MockServerData = {
+    titles: string[];
+    descriptions: string[];
+    previewImages: string[];
+    images: string[];
+    userNames: string[];
+    emails: string[];
+    passwords: string[];
+    avatars: string[];
+};
