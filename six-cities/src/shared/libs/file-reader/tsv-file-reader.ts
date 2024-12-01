6761cbdcd532bef036ad1508a68e51d3e609bb12
kirill-cite <kirill.kysh06041996@yandex.ru>
2024-12-01 12:47:20 +0300

    2.25 Добавляет потоки для чтения. WIP

--- six-cities/src/shared/libs/file-reader/tsv-file-reader.ts
+++ six-cities/src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,4 +1,5 @@
 import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
 
 import { FileReader } from './file-reader.interface.js';
 import { Offer, City, User, Location } from '../../types/index.js';
@@ -6,25 +7,12 @@ import { Offer, City, User, Location } from '../../types/index.js';
 import dayjs from 'dayjs';
 
 export class TSVFileReader extends EventEmitter implements FileReader {
-  private rawData = '';
+  private CHUNK_SIZE = 16384;
 
   constructor(
     private readonly filename: string
   ) {
     super();
-  }
-
-  private validateRawData() {
-    if (! this.rawData) {
-      throw new Error('file was not read');
-    }
-  }
-
-  private parseRawDataToOffers(): Offer[] {
-    return this.rawData
-      .split('\n')
-      .filter((line) => line.trim().length > 0)
-      .map((line) => this.parseLineToOffer(line));
   }
 
   private parseLineToOffer(line: string): Offer {
@@ -109,12 +97,29 @@ export class TSVFileReader extends EventEmitter implements FileReader {
     };
   }
 
-  public read(): void {
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: this.CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
 
-  }
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
 
-  public toArray(): Offer[] {
-    this.validateRawData();
-    return this.parseRawDataToOffers();
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        const parsedOffer = this.parseLineToOffer(completeRow);
+        this.emit('line', parsedOffer);
+      }
+    }
+
+    this.emit('end', importedRowCount);
   }
 }
