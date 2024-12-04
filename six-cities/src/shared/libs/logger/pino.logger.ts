0a249b57e54780cbef4968133ac8d7f6ce5fd7e2
kirill-cite <kirill.kysh06041996@yandex.ru>
2024-12-04 15:24:44 +0300

    3.25 Добавит вывод сообщения в PinoLogger

--- six-cities/src/shared/libs/logger/pino.logger.ts
+++ six-cities/src/shared/libs/logger/pino.logger.ts
@@ -30,6 +30,7 @@ export class PinoLogger implements Logger {
     });
 
     this.logger = pino({}, multiTransport);
+    this.logger.info('Logger created...');
   }
 
   public debug(message: string, ...args: unknown[]): void {
