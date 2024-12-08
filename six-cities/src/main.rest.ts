26129675988b831960869e0d2981ce8a483c3192
kirill-cite <kirill.kysh06041996@yandex.ru>
2024-12-08 10:52:45 +0300

    4.25 Добавит внедрение зависимостей в DefaultUserService. WIP

--- six-cities/src/main.rest.ts
+++ six-cities/src/main.rest.ts
@@ -2,12 +2,15 @@ import 'reflect-metadata';
 import { Container } from 'inversify';
 
 import { RestApplication } from './rest/index.js';
-
 import { Component } from './shared/types/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
+import { createUserContainer } from './shared/modules/user/index.js';
 
 async function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer());
+  const appContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer()
+  );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
