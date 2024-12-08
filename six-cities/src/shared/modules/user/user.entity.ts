4efea8a4daffee9a7206584f0b0391f49ffc6c4f
kirill-cite <kirill.kysh06041996@yandex.ru>
2024-12-08 09:54:38 +0300

    4.20.2 Добавит методы в UserEntity

--- six-cities/src/shared/modules/user/user.entity.ts
+++ six-cities/src/shared/modules/user/user.entity.ts
@@ -1,24 +1,51 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
+import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
 
 import { User } from '../../types/index.js';
+import { createSHA256 } from '../../helpers/index.js';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
-  @prop({ unique: true, required: true })
+  @prop({ unique: true, required: true, default: '' })
   public name: string;
 
   @prop({ unique: true, required: true })
   public email: string;
 
   @prop({ required: false, default: '' })
-  public avatar?: string | undefined;
+  public avatar: string;
 
-  @prop({ required: true })
-  public password: string;
+  @prop({ required: true, default: '' })
+  public password?: string;
 
-  @prop({ required: true })
+  @prop({ required: true, default: false })
   public isPro: boolean;
+
+  constructor(userData: User) {
+    super();
+
+    this.email = userData.email;
+    this.avatar = userData.avatar;
+    this.name = userData.name;
+    this.isPro = userData.isPro;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
